b0b7213300a429daa70f33cd0931d59682f886c5
future-highway <113635015+future-highway@users.noreply.github.com>
2024-05-14 13:56:08 -0400

    ✨ `deterministic_state_machine`

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,30 @@
 
 use proc_macro::TokenStream;
 
+mod deterministic_state_machine;
 mod event_driven_finite_state_machine;
 mod event_enum;
 mod event_trait;
 mod state_enum;
 mod state_trait;
 
-/// Define an event driven finite state machine.
+/// Build an event driven finite state machine.
 #[proc_macro]
 pub fn event_driven_finite_state_machine(
     input: TokenStream,
 ) -> TokenStream {
     event_driven_finite_state_machine::event_driven_finite_state_machine(input)
+}
+
+/// Build a deterministic finite state machine.
+/// Since the state machine is deterministic (i.e. there is
+/// only one possible transition for each state),
+/// the transitions are enforced at compile time.
+#[proc_macro]
+pub fn deterministic_state_machine(
+    input: TokenStream,
+) -> TokenStream {
+    deterministic_state_machine::deterministic_state_machine(
+        input,
+    )
 }
