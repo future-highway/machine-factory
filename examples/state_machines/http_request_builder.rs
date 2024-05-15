a1a22b6b978f200e51d2c335c7fe75667861dfcd
future-highway <113635015+future-highway@users.noreply.github.com>
2024-05-15 11:47:19 -0400

    ✨ feat(deterministic_state_machine): Simplify and allow multiple transitions
    BREAKING CHANGE: 💥️ `deterministic_state_machine`  input changes

--- examples/state_machines/http_request_builder.rs
+++ examples/state_machines/http_request_builder.rs
@@ -8,9 +8,12 @@
 //! required data to make an HTTP request, and then
 //! sends the request. The user cannot call `send` until all
 //! the required data is set.
+#![allow(dead_code)]
 
+use anyhow::Context;
 use machine_factory::deterministic_state_machine;
 use reqwest::{header::HeaderMap, Method, Url};
+use serde::Serialize;
 
 #[derive(Debug)]
 pub struct HttpRequestBuilderData {
@@ -39,120 +42,103 @@ pub struct Response {
     pub status_code: u16,
 }
 
-impl HttpRequestStateTrait for Response {}
-
 deterministic_state_machine!(pub HttpRequest {
     context: HttpRequestBuilderData,
+    // Optionally specify a trait that all states must implement
     state_trait: trait HttpRequestStateTrait {},
-    // This is an optional field
-    acceptor_trait: HttpRequestAcceptorTrait,
-    // All transitions functions are implemented in this struct, either directly or through the acceptor_trait (if provided)
-    acceptor_struct: HttpRequestAcceptor,
-    error: Box<&'static str>,
     transitions: [
-        NeedsMethod.set_method(method: Method) ->
-        NeedsUrl.set_url(url: Url) ->
-        NeedsHeaders.set_headers(headers: HeaderMap) ->
-        NeedsBody.set_body(body: Option<Vec<u8>>)? ->
-        Pending.send().await?reqwest::Error -> Response
+        NeedsMethod {
+            pub fn set_method(self, method: Method) -> HttpRequest<NeedsUrl> {
+                let Self { mut context, .. } = self;
+                context.method = Some(method);
+                HttpRequest { context, state: NeedsUrl }
+            }
+        },
+        NeedsUrl {
+            pub fn set_url(self, url: Url) -> HttpRequest<NeedsHeaders> {
+                let Self { mut context, .. } = self;
+                context.url = Some(url);
+                HttpRequest { context, state: NeedsHeaders }
+            }
+        },
+        NeedsHeaders {
+            pub fn set_headers(self, headers: HeaderMap) -> HttpRequest<NeedsBody> {
+                let Self { mut context, .. } = self;
+                context.headers = Some(headers);
+                HttpRequest { context, state: NeedsBody }
+            }
+        },
+        NeedsBody {
+            pub fn no_body(self) -> HttpRequest<Pending> {
+                self.set_body(None).expect("setting body failed")
+            }
+
+            pub fn set_body(self, body: Option<Vec<u8>>) -> Result<HttpRequest<Pending>, &'static str> {
+                if let Some(body) = &body {
+                    if body.len() > 1024 {
+                        return Err("body too large");
+                    }
+                }
+
+                let HttpRequest { mut context, .. } = self;
+                context.body = body;
+                Ok(HttpRequest { context, state: Pending })
+            }
+
+            pub fn set_json_body<Body: Serialize>(self, body: &Body) -> anyhow::Result<HttpRequest<Pending>> {
+                let body = serde_json::to_vec(body).context("failed to serialize body")?;
+                self.set_body(Some(body)).map_err(|e| anyhow::anyhow!(e)).context("failed to set body")
+            }
+
+            pub async fn send(self) -> Result<Response, reqwest::Error> {
+                self.no_body().send().await
+            }
+        },
+        Pending {
+            pub async fn send(self) -> Result<Response, reqwest::Error> {
+                let HttpRequest { mut context, .. } = self;
+                let HttpRequestBuilderData {
+                    method,
+                    url,
+                    headers,
+                    body,
+                } = &mut context;
+
+                let url = url.take().expect("url not set");
+
+                let request = reqwest::Client::new();
+                let request = match method
+                    .take()
+                    .expect("method not set")
+                {
+                    Method::GET => request.get(url),
+                    Method::POST => request.post(url),
+                    Method::PUT => request.put(url),
+                    Method::DELETE => request.delete(url),
+                    #[allow(clippy::unimplemented)]
+                    _ => unimplemented!(
+                        "Implement the rest, or make sure the state machine is not used with unsupported methods"
+                    ),
+                };
+
+                let request = request.headers(
+                    headers.take().expect("headers not set"),
+                );
+
+                let request = if let Some(body) = body.take() {
+                    request.body(body)
+                } else {
+                    request
+                };
+
+                let response = request.send().await?;
+                let status_code = response.status().as_u16();
+
+                Ok(Response { status_code })
+            }
+        },
     ]
 });
-
-#[allow(dead_code)]
-struct HttpRequestAcceptor;
-
-impl HttpRequestAcceptorTrait for HttpRequestAcceptor {
-    fn set_method(
-        state: HttpRequest<NeedsMethod>,
-        method: Method,
-    ) -> HttpRequest<NeedsUrl> {
-        let HttpRequest { mut context, .. } = state;
-        context.method = Some(method);
-        HttpRequest { context, state: NeedsUrl }
-    }
-
-    fn set_url(
-        state: HttpRequest<NeedsUrl>,
-        url: Url,
-    ) -> HttpRequest<NeedsHeaders> {
-        let HttpRequest { mut context, .. } = state;
-        context.url = Some(url);
-        HttpRequest { context, state: NeedsHeaders }
-    }
-
-    fn set_headers(
-        state: HttpRequest<NeedsHeaders>,
-        head: HeaderMap,
-    ) -> HttpRequest<NeedsBody> {
-        let HttpRequest { mut context, .. } = state;
-        context.headers = Some(head);
-        HttpRequest { context, state: NeedsBody }
-    }
-
-    fn set_body(
-        state: HttpRequest<NeedsBody>,
-        body: Option<Vec<u8>>,
-    ) -> Result<HttpRequest<Pending>, Box<&'static str>>
-    {
-        if let Some(body) = &body {
-            if body.len() > 1024 {
-                return Err(Box::new("body too large"));
-            }
-        }
-
-        let HttpRequest { mut context, .. } = state;
-        context.body = body;
-        Ok(HttpRequest { context, state: Pending })
-    }
-
-    #[allow(dead_code)]
-    async fn send(
-        state: HttpRequest<Pending>,
-    ) -> Result<HttpRequest<Response>, reqwest::Error> {
-        let HttpRequest { mut context, .. } = state;
-        let HttpRequestBuilderData {
-            method,
-            url,
-            headers,
-            body,
-        } = &mut context;
-
-        let url = url.take().expect("url not set");
-
-        let request = reqwest::Client::new();
-        let request = match method
-            .take()
-            .expect("method not set")
-        {
-            Method::GET => request.get(url),
-            Method::POST => request.post(url),
-            Method::PUT => request.put(url),
-            Method::DELETE => request.delete(url),
-            #[allow(clippy::unimplemented)]
-            _ => unimplemented!(
-                "Implement the rest, or make sure the state machine is not used with unsupported methods"
-            ),
-        };
-
-        let request = request.headers(
-            headers.take().expect("headers not set"),
-        );
-
-        let request = if let Some(body) = body.take() {
-            request.body(body)
-        } else {
-            request
-        };
-
-        let response = request.send().await?;
-        let status_code = response.status().as_u16();
-
-        Ok(HttpRequest {
-            context,
-            state: Response { status_code },
-        })
-    }
-}
 
 impl Default for HttpRequest<NeedsMethod> {
     fn default() -> Self {
